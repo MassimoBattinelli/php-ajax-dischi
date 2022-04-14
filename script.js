75f4948bda33b84007fa57c6a31b1d9d157693ab
massi <massimo.battinelli@gmail.com>
2022-04-14 10:53:03 +0200

    fine progetto

---
+++ script.js
@@ -0,0 +1,16 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    dischi: [],
+  },
+  methods: {
+    getCall() {
+      axios.get("./dischi.php").then((res) => {
+        this.dischi = res.data;
+      });
+    },
+  },
+  created() {
+    this.getCall();
+  },
+});
